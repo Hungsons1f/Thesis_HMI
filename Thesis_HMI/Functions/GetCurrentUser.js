673ce306ae52221126227dd84129df76014a83ed
Cydeus <Hungsons1fphu@gmail.com>
2021-02-24 22:51:17 +0700

    Almost done

---
+++ Thesis_HMI/Functions/GetCurrentUser.js
@@ -0,0 +1,15 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.742.5/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+(function (TcHmi) {
+    var Functions;
+    (function (Functions) {
+        var Thesis_HMI;
+        (function (Thesis_HMI) {
+            function GetCurrentUser(par1) {
+                return TcHmi.Server.getCurrentUser();
+            }
+            Thesis_HMI.GetCurrentUser = GetCurrentUser;
+        })(Thesis_HMI = Functions.Thesis_HMI || (Functions.Thesis_HMI = {}));
+        Functions.registerFunctionEx('GetCurrentUser', 'TcHmi.Functions.Thesis_HMI', Thesis_HMI.GetCurrentUser);
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
