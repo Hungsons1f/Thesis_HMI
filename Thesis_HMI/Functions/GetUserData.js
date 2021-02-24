e742b40af87b35b1aabb3a049943d5d68e45a076
Cydeus <Hungsons1fphu@gmail.com>
2021-02-24 16:25:17 +0700

    Complete the bone

--- Thesis_HMI/Functions/GetUserData.js
+++ Thesis_HMI/Functions/GetUserData.js
@@ -1,41 +1,35 @@
 ﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
 /// <reference path="../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.742.5/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
 (function (TcHmi) {
     var Functions;
     (function (Functions) {
         var Thesis_HMI;
         (function (Thesis_HMI) {
             function GetUserData(par1) {
-                var username;
-                var userdata;
-                var result = "{";
+                TcHmi.Server.UserManagement.listUsernames(function (data) {
+                    if (data.error === TcHmi.Errors.NONE) {
+                        //console.log(data.userList); 
+                        TcHmi.Symbol.writeEx("%i%sUserName%/i%", data.userList);
+                    } else {
+                        // Error
+                        //console.log(data);
+                    }
+                });
 
-                let [username, userdata] = await Promise.all([
-                    TcHmi.Server.UserManagement.listUsernames(function (data) {
+                TcHmi.Server.UserManagement.listUsersEx(
+                    null,
+                    { timeout: 2000 },
+                    function (data) {
                         if (data.error === TcHmi.Errors.NONE) {
-                            username = data.userList;
-                            console.log(data.userList);
+                            //console.log(data.userDetails);
+                            TcHmi.Symbol.writeEx("%i%sUserData%/i%", data.userDetails);
                         } else {
-                            console.log(data);
+                            // Error
+                            //console.log(data);
                         }
-                    }),
-                    TcHmi.Server.UserManagement.listUsers(function (data) {
-                        if (data.error === TcHmi.Errors.NONE) {
-                            userdata = JSON.parse(data.userDetails);
-                            console.log(data.userDetails);
-                        } else {
-                            console.log(data);
-                        }
-                    })
-                ]);
-
-
-
-                for (var i in userdata) {
-                    result = result + username[i] + userdata[i]; 
-                }
-
-                console.log(result);
+                    }
+                );
             }
             Thesis_HMI.GetUserData = GetUserData;
         })(Thesis_HMI = Functions.Thesis_HMI || (Functions.Thesis_HMI = {}));
---
+++ Thesis_HMI/Functions/JsonParse.js
@@ -0,0 +1,30 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.742.5/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+(function (TcHmi) {
+    var Functions;
+    (function (Functions) {
+        var Thesis_HMI;
+        (function (Thesis_HMI) {
+            function JsonParse(userName, userData) {
+                var obj = JSON.parse(userData);
+                var name = JSON.parse(userName);
+                var array = [];
+
+                var i = 0;
+                for (var k in obj) {
+                    var x = {
+                        'name': name[i++],
+                        'enabled': obj[k].enabled,
+                        'autoLogout': obj[k].autoLogout,
+                        'groups': obj[k].groups
+                    }
+                    array.push(x);
+                }
+
+                return array;
+            }
+            Thesis_HMI.JsonParse = JsonParse;
+        })(Thesis_HMI = Functions.Thesis_HMI || (Functions.Thesis_HMI = {}));
+        Functions.registerFunctionEx('JsonParse', 'TcHmi.Functions.Thesis_HMI', Thesis_HMI.JsonParse);
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
